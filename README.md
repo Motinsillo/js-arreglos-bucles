ee9c60b43138934af2b5bb2f95342fa1a4dc162c
Alejandro <abarbozaveliz26@gmail.com>
2025-03-03 02:07:25 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# js-arreglos-bucles
+# code-101-lab10
+
+Template for Javascript Challenges with arrays, loops and functions.
---
+++ challenges.js
@@ -0,0 +1,72 @@
+// Reto 1: Suma de Elementos
+function sumarElementos(arreglo) {
+    
+    let suma = 0
+
+for(i=0; i<arreglo.length; i++){
+    suma=suma+arreglo[i]
+}
+return suma;
+}
+
+// Reto 2: Número Mayor
+function encontrarMayor(arreglo) {
+    // Los estudiantes implementarán esta función
+    // Debe retornar el número más grande del arreglo
+    
+    let mayor = Math.max(...arreglo);
+    return mayor;
+
+}
+
+// Reto 3: Filtrar Pares
+function filtrarPares(arreglo) {
+    // Los estudiantes implementarán esta función
+    // Debe retornar un nuevo arreglo solo con los números pares
+
+    let pares = [];
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] % 2 === 0) {
+      pares.push(arreglo[i]);
+    }
+  }
+  return pares;
+        
+}
+
+// Reto 4: Contar Vocales
+function contarVocales(texto) {
+    
+  let vocales = texto.match(/[aeiou]/gi);
+ return vocales.length;
+}
+
+// Reto 5: Invertir Arreglo
+function invertirArreglo(arreglo) {
+    // Los estudiantes implementarán esta función
+    // Debe retornar un nuevo arreglo con los elementos en orden inverso
+}
+
+// Reto 6: Promedio de Notas
+function calcularPromedio(notas) {
+    // Los estudiantes implementarán esta función
+    // Debe retornar el promedio de las notas
+}
+
+// Reto 7: Contador de Letras
+function contarLetra(texto, letra) {
+    // Los estudiantes implementarán esta función
+    // Debe retornar cuántas veces aparece la letra en el texto
+}
+
+// Reto 8: Elementos Únicos
+function obtenerUnicos(arreglo) {
+    // Los estudiantes implementarán esta función
+    // Debe retornar un nuevo arreglo sin elementos duplicados
+}
+
+// Reto 9: Palíndromo
+function esPalindromo(texto) {
+    // Los estudiantes implementarán esta función
+    // Debe retornar true si el texto es un palíndromo, false si no
+}
